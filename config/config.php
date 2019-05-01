adf338d437254c2beb90370634a3e4b4aa1f8205
pandemuliada <pandemuliada@gmail.com>
2019-05-02 00:45:22 +0800

    configuration file

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define('BASE_URL', 'http://localhost/UKKInventaris2');
+  
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'ukkinventaris');
